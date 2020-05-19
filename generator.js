162ee6c18a2153778c54405d4522583f907a91a3
Rodrigo Burgos S <rodrigoburgoscl@gmail.com>
2020-05-19 09:59:21 -0400

    🚧 Mejora de generator

--- generator.js
+++ generator.js
@@ -1,21 +1,23 @@
-function nextView() {
-	var home = document.getElementById("homeView");
-	var diploma = document.getElementById("diplomaView");
-	home.style.display='none';
-	diploma.style.display='block';
+const nextViewBtn = document.querySelector('#homeView button');
+const generatePDFBtn = document.querySelector('#diplomaView button');
+const result = document.querySelector("#resultado");
+var diploma = document.querySelector("#diplomaView").style;
+
+const nextView = (personName = document.querySelector("#inputName").value.trim()) => {
+	if (!personName) return;
+	const home = document.querySelector("#homeView").style;
+	result.innerHTML = personName;
+	home.display = 'none';
+	diploma.display = 'block';
 }
 
-function generatePDF() {
-	var personName = document.getElementById("inputName").value;
-
-	var doc = new jsPDF({
-		orientation: 'landscape'
-	});
-
-	doc.text(20, 20, personName);
-
-	doc.save(personName + '.pdf');
-
-	document.getElementById("resultado").innerHTML = personName;
+const generatePDF = () => {
+	const options = { orientation: 'landscape' };
+	const doc = new jsPDF(options);
+	const { innerHTML } = result;
+	doc.fromHTML(diploma, 20, 20);
+	doc.save(`diploma-${ innerHTML }.pdf`);
 }
 
+nextViewBtn.addEventListener('click', () => nextView());
+generatePDFBtn.addEventListener('click', () => generatePDF());
